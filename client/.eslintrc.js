46475b1462724629d03c37f98c4d24db85ee04f8
Igor S <tombcold@gmail.com>
2022-02-24 17:48:29 +0300

    initProject

---
+++ client/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'react/jsx-filename-extension': [0, { extensions: ['.js', '.jsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'comma-dangle': ['error', 'never'],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'default-param-last': 'off',
+    'import/no-unresolved': 'off',
+  },
+};
---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { store } from './redux/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+axios.defaults.baseURL = 'http://localhost:3001';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
